114b54fb27947aad536968de56e3ffb59b34f411
David <daalmega@gmail.com>
2022-05-03 23:49:31 -0500

    Format file

--- agro_app/lib/main.dart
+++ agro_app/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:agro_app/Screens/Home/Client/home.dart';
 import 'package:agro_app/Screens/Login/Client/login_screen.dart';
 import 'package:agro_app/Screens/Login/Store/login_screen.dart';
+import 'package:agro_app/Screens/Pedidos/Client/login_screen.dart';
 import 'package:agro_app/Screens/Welcome/welcome_screen.dart';
 import 'package:agro_app/constants.dart';
 import 'package:flutter/material.dart';
@@ -55,17 +56,38 @@ class BodyLayout extends StatelessWidget {
 }
 
 Widget _myListView(BuildContext context) {
+  final titles = [
+    'Welcome',
+    'Login Vendedor',
+    'Login Comprador',
+    'Sing Up Comprador',
+    'Sing Up Vendedor',
+    'Info Tienda',
+    'Info Tienda 2',
+    'Home',
+    'Pedidos Comprador'
+  ];
+  final pags = [
+    const WelcomeScreen(),
+    const LoginScreenV(),
+    const LoginScreenC(),
+    const SignUpScreenC(),
+    const SignUpScreenV(),
+    const SignUpScreen2V(),
+    const SignUpScreen3V(),
+    const Home(),
+    const PedidosC(),
+  ];
 
-  final titles = ['Welcome','Login Vendedor','Login Comprador','Sing Up Comprador','Sing Up Vendedor', 'Info Tienda', 'Info Tienda 2', 'Home'];
-  final pags = [const WelcomeScreen(), const LoginScreenV(), const LoginScreenC(),const SignUpScreenC(), const SignUpScreenV(), const SignUpScreen2V(), const SignUpScreen3V(), const Home(), ];
-  
   return ListView.builder(
     itemCount: titles.length,
     itemBuilder: (context, index) {
       return Card(
         child: ListTile(
           title: Text(titles[index]),
-          onTap: (){Get.to(() => pags[index]);},
+          onTap: () {
+            Get.to(() => pags[index]);
+          },
         ),
       );
     },
