395dbf9d9bd9610b1aa8dfd3370f9f763053b022
sleepypower <daalmega@gmail.com>
2022-05-25 15:54:52 -0500

    Add Carrito de Compras

--- agro_app/lib/Screens/Home/Client/cart.dart
+++ agro_app/lib/Screens/Home/Client/cart.dart
@@ -1,4 +1,3 @@
-
 import 'package:agro_app/constants.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -8,7 +7,14 @@ class CarritoDeCompras extends StatelessWidget {
   // ignore: prefer_const_constructors_in_immutables
   CarritoDeCompras({Key? key}) : super(key: key);
 
-  final prodnames = ['Papa Pastusa','Aguacate','Huevos','Huevos','Huevos','Huevos'];
+  final prodnames = [
+    'Papa Pastusa',
+    'Aguacate',
+    'Huevos',
+    'Huevos',
+    'Huevos',
+    'Huevos'
+  ];
   final asimg = [
     'assets/Images/dilema-papa.jpg',
     'assets/Images/Aguacate.jpg',
@@ -16,7 +22,8 @@ class CarritoDeCompras extends StatelessWidget {
     'assets/Images/Huevo.jpg',
     'assets/Images/Huevo.jpg',
     'assets/Images/Huevo.jpg',
-    ];
+  ];
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -25,141 +32,178 @@ class CarritoDeCompras extends StatelessWidget {
         child: Container(
           color: kWhiteColor,
           child: Column(
-            children:  <Widget>[
-              SizedBox(height: size.height*0.05,),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Row(
-                  //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        Get.back();
-                      },
-                      child: const Icon(CupertinoIcons.back, color: kPrimaryColor, size: 30,),
-                      
+            children: <Widget>[
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              Row(
+                //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: () {
+                      Get.back();
+                    },
+                    child: const Icon(
+                      CupertinoIcons.back,
+                      color: kPrimaryColor,
+                      size: 30,
                     ),
-                    const Text(
-                      'Carrito de Compras',
-                      style: TextStyle(
-                        color: kPrimaryColor,
-                        fontSize: 25.0,
-                        fontWeight: FontWeight.w600,
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(right: 30.0),
+                      child: Center(
+                        child: Text(
+                          'Carrito de Compras',
+                          style: TextStyle(
+                            color: kPrimaryColor,
+                            fontSize: 25.0,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
                       ),
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-              Flexible(flex: 2, child: Padding(
-                padding: const EdgeInsets.all(32.0),
-                child: ListView.builder(
-                  itemCount: 50,
-
-                  itemBuilder: (context, index) {
-                    return Container(
-                      height: 100,
-                      margin: const EdgeInsets.symmetric(vertical: 5),
-                      decoration: const BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(15)),
-                                  color: Colors.grey
+              Flexible(
+                flex: 2,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 32.0, right: 32, top: 16),
+                  child: ListView.builder(
+                    itemCount: 50,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 100,
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        margin: const EdgeInsets.symmetric(vertical: 5),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            color: kGrayColor),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Flexible(
+                                flex: 1,
+                                child: Image.asset(
+                                  asimg[0],
+                                  fit: BoxFit.fitHeight,
                                 ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                  children: [
-                                  const Icon(CupertinoIcons.photo),
-                                  Column(
-                                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              ),
+                              Expanded(
+                                child: Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 5.0),
+                                  child: Column(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
                                     children: [
-                                      const Text("Product Name"),
+                                      const Text("Product Name",
+                                          overflow: TextOverflow.ellipsis,
+                                          maxLines: 2),
                                       // ignore: avoid_unnecessary_containers
                                       Container(
                                         child: const Text("\$Price"),
                                       )
                                     ],
                                   ),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    children: const [
-                                      Icon(CupertinoIcons.minus_circle),
-                                      Text("1"),
-                                      Icon(CupertinoIcons.add_circled),
-                                    ],
-                                  ),
-                                ]),
-                    );
-                  },
+                                ),
+                              ),
+                              Flexible(
+                                flex: 1,
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    GestureDetector(
+                                        onTap: () {
+                                          print("Less");
+                                        },
+                                        child:
+                                            Icon(CupertinoIcons.minus_circle)),
+                                    Text("1"),
+                                    GestureDetector(
+                                        onTap: () {
+                                          print("More");
+                                        },
+                                        child:
+                                            Icon(CupertinoIcons.add_circled)),
+                                  ],
+                                ),
+                              ),
+                            ]),
+                      );
+                    },
+                  ),
                 ),
-              ),),
+              ),
               Container(
                 decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(15)),
-                                color: Colors.deepPurple
-                              ),
-                              padding: const EdgeInsets.symmetric(vertical: 10),
+                    borderRadius: BorderRadius.only(topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+                    color: Color(0xff0A1A3B)),
+                padding: const EdgeInsets.symmetric(vertical: 10),
                 child: Column(
                   children: [
                     Row(
                       children: [
                         Expanded(
-                          child: GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              child: const Center(child: Text("CheckOut", style: TextStyle(color: kWhiteColor, fontWeight: FontWeight.bold),)),
-                              padding: const EdgeInsets.symmetric(vertical: 25),
-                              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(15)),
-                                color: Colors.red
-                              ),
-                            ),
-                          ),
-                          
+                          child: const Center(
+                              child: Text(
+                            "Total: \$2389",
+                            style: TextStyle(
+                                color: kWhiteColor,
+                                fontWeight: FontWeight.bold),
+                          )),
                         ),
                         Expanded(
                           child: GestureDetector(
                             onTap: () {},
                             child: Container(
-                              child: const Center(child: Text("CheckOut", style: TextStyle(color: kWhiteColor, fontWeight: FontWeight.bold),)),
+                              child: const Center(
+                                  child: Text(
+                                "Clean Cart",
+                                style: TextStyle(
+                                    color: kWhiteColor,
+                                    fontWeight: FontWeight.bold),
+                              )),
                               padding: const EdgeInsets.symmetric(vertical: 25),
-                              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+                              margin: const EdgeInsets.symmetric(
+                                  horizontal: 10, vertical: 5),
                               decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(15)),
-                                color: Colors.red
-                              ),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(15)),
+                                  color: kRedColor),
                             ),
                           ),
-
-                          
                         ),
-                        
                       ],
                     ),
                     GestureDetector(
-                            onTap: () {},
-                            child: Container(
-                              child: const Center(child: Text("CheckOut", style: TextStyle(color: kWhiteColor, fontWeight: FontWeight.bold),)),
-                              padding: const EdgeInsets.symmetric(vertical: 25),
-                              margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-                              decoration: const BoxDecoration(
-                                borderRadius: BorderRadius.all(Radius.circular(15)),
-                                color: Colors.red
-                              ),
-                            ),
-                          ),
-
+                      onTap: () {},
+                      child: Container(
+                        child: const Center(
+                            child: Text(
+                          "CheckOut",
+                          style: TextStyle(
+                              color: kWhiteColor, fontWeight: FontWeight.bold),
+                        )),
+                        padding: const EdgeInsets.symmetric(vertical: 25),
+                        margin: const EdgeInsets.symmetric(
+                            horizontal: 10, vertical: 5),
+                        decoration: const BoxDecoration(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                            color: kPrimaryColor),
+                      ),
+                    ),
                   ],
                 ),
               )
-              
-              
-              
-          
             ],
           ),
         ),
       ),
-      
     );
   }
 }
